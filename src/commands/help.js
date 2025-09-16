f516f5bc6061d6b5228193c215550658d1d124e2
LittleMoki <steamlittle659@gmail.com>
2025-09-16 11:31:25 +0500

    change support

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,7 +1,7 @@
 export default function helpCommand(bot) {
 	bot.hears('ℹ️ Yordam', async ctx => {
 		try {
-			await ctx.reply(`🆘 Yordam kerakmi? \n\n👉 @nurullayev_me`)
+			await ctx.reply(`🆘 Yordam kerakmi? \n\n👉 @indiga_management`)
 		} catch (error) {
 			console.error('Help error:', error)
 			await ctx.reply("Xatolik yuz berdi. Iltimos, keyinroq urunib ko'ring.")
